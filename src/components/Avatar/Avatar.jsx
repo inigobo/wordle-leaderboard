004cb3222690e48b7c9b6dccdf3ac8517a4f0c00
inigobo <inniber@gmail.com>
2023-01-24 09:28:51 +0100

    add board modal

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -2,14 +2,13 @@ import { styled } from "@stitches/react";
 import { Image } from "react-bootstrap";
 import { AvatarStyles } from './Avatar.styles';
 import { getSGV } from "../../services/apiCalls";
-import { useState, useEffect } from "react";
 
-const Avatar = (props) => {
-return (
-    <AvatarContainer size={props.variant}>
-        <Image src={getSGV(props.seed)} />
-    </AvatarContainer>
-);
+const Avatar = ({ variant, seed }) => {
+    return (
+        <AvatarContainer size={variant}>
+            <Image src={getSGV(seed)} />
+        </AvatarContainer>
+    );
 }
 
 export default Avatar;
